27c5c8a3d3e220a4ea8420154d722855c78b2430
Jiaming Guo <gjm.mailbox2@gmail.com>
2021-04-19 13:48:38 -0400

    added security

---
+++ src/main/java/com/example/jsstoreapi/controllers/UsersController.java
@@ -0,0 +1,47 @@
+package com.example.jsstoreapi.controllers;
+
+import com.example.jsstoreapi.CustomizedResponse;
+import com.example.jsstoreapi.models.UserModel;
+import com.example.jsstoreapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+
+@Controller
+public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity getUsers() {
+        var response = new CustomizedResponse("A list of all users", userService.getUsers());
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity getAUser(@PathVariable("id") String id) {
+        var response = new CustomizedResponse("User with id "+id, Collections.singletonList(userService.getAUser(id)));
+        return new ResponseEntity(response, HttpStatus.OK);
+
+    }
+
+    @PostMapping(value="/users", consumes={MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity createUser(@RequestBody UserModel user) {
+        var response = new CustomizedResponse("User created successfully", Collections.singletonList(userService.addUser(user)));
+        return new ResponseEntity(response, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity deleteUser(@PathVariable("id") String id) {
+        userService.deleteUser(id);
+        var response = new CustomizedResponse("delete success", null);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+}
